82aa7c4157167a001e034a3d66308894f7d1465c
PabloS <jsanc016@uottawa.ca>
2022-06-30 10:48:45 -0400

    PNG -> png test

--- src/pages/TutorsComponents/CatherineTutor.js
+++ src/pages/TutorsComponents/CatherineTutor.js
@@ -18,7 +18,7 @@ function About() {
                         </Col>
 
                         <Col>
-                            <img src='./InstituteVirtual.github.io/assets/tutor_2.PNG'></img>
+                            <img src='./InstituteVirtual.github.io/assets/tutor_2.png'></img>
                         </Col>
 
                         
